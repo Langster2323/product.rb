2fb4eb03513fe5845d1a88a390e93d026d60d9e0
Ahkeem Lang <langahkeem@gmail.com>
2016-07-11 16:39:14 -0500

    I created a index and show action method for this assignment.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,17 +1,18 @@
 class ProductController < ApplicationController
   #CRUD
   def index
+    render 'product/index.html.erb', locals: {
+      product: Product.all
+    }
   end
 
   def show
-  end
-
-  def created
-  end
-
-  def update
-  end
-
-  def delete
+    if Product.exists?(params[:id])
+      render template: 'product/show.html.erb', locals: {
+        product: Product.find(params[:id])
+      }
+    else
+      render html: "Not Found", status: 404
+    end
   end
 end
